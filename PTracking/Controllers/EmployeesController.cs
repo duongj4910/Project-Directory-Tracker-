54dcc2dfdf35c5e193e8a6cddccf17f54ae3e963
Jenny D <127423421+duongj4910@users.noreply.github.com>
2023-12-12 00:13:17 -0600

    restore changes terry lost

--- PTracking/Controllers/EmployeesController.cs
+++ PTracking/Controllers/EmployeesController.cs
@@ -43,96 +43,103 @@ namespace PTracking.Controllers
             return View(employee);
         }
 
-        // GET: Employees/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
 
-        // POST: Employees/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ID,Name,Email,ProjectsAssigned,ClientsAssigned,Phone,TimeZone,DisctinctRole,Role,TeamLead,TeamId,ProjectManager,Availability,icon,PTO")] Employee employee)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(employee);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(employee);
-        }
+		// GET: Category/AddOrEdit
+		public IActionResult AddOrEdit(int id = 0)
+		{
+			if (id == 0)
+				return View(new Employee());
+			else
+				return View(_context.Employee.Find(id));
+
+		}
+
+		// POST: Category/AddOrEdit
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> AddOrEdit([Bind("ID,Name,Email,ProjectsAssigned,ClientsAssigned,Phone,TimeZone,DisctinctRole,Role,TeamLead,TeamId,ProjectManager,Availability,icon,PTO")] Employee employee)
+		{
+			if (ModelState.IsValid)
+			{
+				if (employee.ID == 0)
+					_context.Add(employee);
+				else
+					_context.Update(employee);
+				await _context.SaveChangesAsync();
+				return RedirectToAction(nameof(Index));
+			}
+			return View(employee);
+		}
+
+		//// POST: Employees/Create
+		//// To protect from overposting attacks, enable the specific properties you want to bind to.
+		//// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		//[HttpPost]
+  //      [ValidateAntiForgeryToken]
+  //      public async Task<IActionResult> Create([Bind("ID,Name,Email,ProjectsAssigned,ClientsAssigned,Phone,TimeZone,DisctinctRole,Role,TeamLead,TeamId,ProjectManager,Availability,icon,PTO")] Employee employee)
+  //      {
+  //          if (ModelState.IsValid)
+  //          {
+  //              _context.Add(employee);
+  //              await _context.SaveChangesAsync();
+  //              return RedirectToAction(nameof(Index));
+  //          }
+  //          return View(employee);
+  //      }
 
         // GET: Employees/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.Employee == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Edit(int? id)
+        //{
+        //    if (id == null || _context.Employee == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var employee = await _context.Employee.FindAsync(id);
-            if (employee == null)
-            {
-                return NotFound();
-            }
-            return View(employee);
-        }
+        //    var employee = await _context.Employee.FindAsync(id);
+        //    if (employee == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return View(employee);
+        //}
 
         // POST: Employees/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Email,ProjectsAssigned,ClientsAssigned,Phone,TimeZone,DisctinctRole,Role,TeamLead,TeamId,ProjectManager,Availability,icon,PTO")] Employee employee)
-        {
-            if (id != employee.ID)
-            {
-                return NotFound();
-            }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Email,ProjectsAssigned,ClientsAssigned,Phone,TimeZone,DisctinctRole,Role,TeamLead,TeamId,ProjectManager,Availability,icon,PTO")] Employee employee)
+        //{
+        //    if (id != employee.ID)
+        //    {
+        //        return NotFound();
+        //    }
 
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(employee);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!EmployeeExists(employee.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(employee);
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        try
+        //        {
+        //            _context.Update(employee);
+        //            await _context.SaveChangesAsync();
+        //        }
+        //        catch (DbUpdateConcurrencyException)
+        //        {
+        //            if (!EmployeeExists(employee.ID))
+        //            {
+        //                return NotFound();
+        //            }
+        //            else
+        //            {
+        //                throw;
+        //            }
+        //        }
+        //        return RedirectToAction(nameof(Index));
+        //    }
+        //    return View(employee);
+        //}
 
-        // GET: Employees/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.Employee == null)
-            {
-                return NotFound();
-            }
-
-            var employee = await _context.Employee
-                .FirstOrDefaultAsync(m => m.ID == id);
-            if (employee == null)
-            {
-                return NotFound();
-            }
-
-            return View(employee);
-        }
 
         // POST: Employees/Delete/5
         [HttpPost, ActionName("Delete")]
@@ -159,3 +166,21 @@ namespace PTracking.Controllers
         }
     }
 }
+
+// GET: Employees/Delete/5
+//public async Task<IActionResult> Delete(int? id)
+//{
+//	if (id == null || _context.Employee == null)
+//	{
+//		return NotFound();
+//	}
+
+//	var employee = await _context.Employee
+//		.FirstOrDefaultAsync(m => m.ID == id);
+//	if (employee == null)
+//	{
+//		return NotFound();
+//	}
+
+//	return View(employee);
+//}
